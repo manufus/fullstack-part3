886156b3ac8f30d62fefe90ccee6b376a4d3874e
Manuel Hortelano Rodríguez <manuel.hortelano.rodriguez@gmail.com>
2025-02-10 19:14:35 +0100

    Latest functionality with database and validations

--- index.js
+++ index.js
@@ -1,68 +1,100 @@
+require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
 const app = express();
+const Person = require("./models/person");
 
+app.use(express.static("dist"));
+app.use(express.json());
+app.use(cors());
+
+// app.use(morgan("tiny"));
+app.use(morgan(":method :url :status :response-time :object"));
 morgan.token("object", function (req, res) {
   return JSON.stringify(req.body);
 });
 
-app.use(express.json());
-app.use(express.static("dist"));
+const errorHandler = (error, request, response, next) => {
+  console.log(error.message);
 
-app.use(cors());
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  } else if (error.name === "ValidationError") {
+    return response.status(400).json({ error: error.message });
+  }
 
-app.use(morgan("tiny"));
-app.use(morgan(":method :url :status :response-time :object"));
+  next(error);
+};
 
-let persons = [
-  {
-    id: "1",
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: "2",
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: "3",
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: "4",
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
+// let persons = [
+//   {
+//     id: "1",
+//     name: "Arto Hellas",
+//     number: "040-123456",
+//   },
+//   {
+//     id: "2",
+//     name: "Ada Lovelace",
+//     number: "39-44-5323523",
+//   },
+//   {
+//     id: "3",
+//     name: "Dan Abramov",
+//     number: "12-43-234345",
+//   },
+//   {
+//     id: "4",
+//     name: "Mary Poppendieck",
+//     number: "39-23-6423122",
+//   },
+// ];
 
-app.get("/", (request, response) => {
-  response.send("<h1>Phonebook backend</h1>");
-});
+// app.get("/", (request, response) => {
+//   response.send("<h1>Phonebook backend</h1>");
+// });
 
-app.get("/api/persons", (request, response) => {
-  response.json(persons);
+app.get("/api/persons", (request, response, next) => {
+  Person.find({}).then((people) => {
+    response.json(people);
+  });
 });
 
 app.get("/info", (request, response) => {
-  response.send(
-    `<p>Phonebook has info for ${persons.length} people <br/>${new Date()}</p>`
-  );
+  Person.find({})
+    .then((result) => {
+      const count = result.length;
+      response.send(
+        `<p>Phonebook has info for ${count} people <br/>${new Date()}</p>`
+      );
+    })
+    .catch((error) => {
+      next(error);
+      response.status(500).send("An error occurred while fetching data.");
+    });
 });
 
-app.get("/api/persons/:id", (request, response) => {
-  const id = request.params.id;
-  const person_info = persons.find((person) => person.id === id);
-  person_info ? response.json(person_info) : response.status(404).end();
+app.get("/api/persons/:id", (request, response, next) => {
+  // const id = request.params.id;
+  // const person_info = persons.find((person) => person.id === id);
+  // person_info ? response.json(person_info) : response.status(404).end();
+  Person.findById(request.params.id)
+    .then((person) => {
+      if (person) {
+        response.json(person);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
 });
 
-app.delete("/api/persons/:id", (request, response) => {
-  const id = request.params.id;
-  persons = persons.filter((person) => person.id !== id);
-
-  response.status(204).end();
+app.delete("/api/persons/:id", (request, response, next) => {
+  Person.findByIdAndDelete(request.params.id)
+    .then((result) => {
+      response.status(204).end();
+    })
+    .catch((error) => next(error));
 });
 
 function getRandomInt(min, max) {
@@ -71,27 +103,55 @@ function getRandomInt(min, max) {
   return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled); // The maximum is exclusive and the minimum is inclusive
 }
 
-app.post("/api/persons", (request, response) => {
+app.post("/api/persons", (request, response, next) => {
   const body = request.body;
+  // if (body.name.length < 3) {
+  //   return response.status(400).json({ error: "name is too short" });
+  // }
 
-  if (!body.name || !body.number) {
-    return response.status(400).json({
-      error: "information missing",
-    });
-  }
-  if (persons.find((person) => person.name === body.name)) {
-    return response.status(400).json({
-      error: "this name already exists",
-    });
-  }
-  const person = {
-    id: getRandomInt(5, 100),
+  // if (!body.name || !body.number) {
+  //   return response.status(400).json({
+  //     error: "information missing",
+  //   });
+  // }
+  // if (persons.find((person) => person.name === body.name)) {
+  //   return response.status(400).json({
+  //     error: "this name already exists",
+  //   });
+  // }
+  const person = new Person({
+    // id: getRandomInt(5, 100),
     name: body.name || "John Doe",
     number: body.number || 654321,
-  };
+  });
 
-  persons = persons.concat(person);
-  response.json(person);
+  person
+    .save()
+    .then((savedPerson) => {
+      response.json(savedPerson);
+    })
+    .catch((error) => next(error));
+
+  // persons = persons.concat(person);
+  // response.json(person);
+});
+
+app.put("/api/persons/:id", (request, response, next) => {
+  const body = request.body;
+
+  // if (body.name.length < 3) {
+  //   return response.status(400).json({ error: "name is too short" });
+  // }
+  const person = {
+    name: body.name,
+    number: body.number,
+  };
+  Person.findByIdAndUpdate(request.params.id, person, { runValidators: true })
+    .then((updatedNumber) => {
+      console.log(body.name);
+      response.json(updatedNumber);
+    })
+    .catch((error) => next(error));
 });
 
 app.get("/api/login", function (req, res) {
@@ -108,7 +168,14 @@ app.post("/api/login", function (req, res) {
   res.send(JSON.stringify(person));
 });
 
-const PORT = process.env.PORT || 3001;
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
+
+app.use(unknownEndpoint);
+app.use(errorHandler);
+
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
   console.log(`----`);
---
+++ models/person.js
@@ -0,0 +1,67 @@
+const mongoose = require("mongoose");
+
+mongoose.set("strictQuery", false);
+
+const url = process.env.MONGODB_URI;
+
+console.log("----");
+console.log("connecting to", url);
+
+mongoose
+  .connect(url)
+  .then((result) => {
+    console.log("connected to MongoDB");
+  })
+  .catch((error) => {
+    console.log("error connecting to MongoDB", error.message);
+  });
+
+const personSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    minLength: 3,
+    required: true,
+  },
+  number: {
+    type: String,
+    minLength: 8,
+    required: true,
+    validate: {
+      validator: function (value) {
+        return /^\d{2,3}-\d+$/.test(value);
+      },
+      message: (props) => `${props.value} is not a valid phone number`,
+    },
+  },
+});
+
+const Person = mongoose.model("Person", personSchema);
+
+// const person = new Person({
+//   name: process.argv[3],
+//   number: process.argv[4],
+// });
+
+personSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+
+// if (process.argv.length < 4) {
+//   Person.find({}).then((result) => {
+//     result.forEach((person) => {
+//       console.log(person);
+//     });
+//     mongoose.connection.close();
+//   });
+// } else if (process.argv.length == 5) {
+//   person.save().then((result) => {
+//     console.log(`added ${person.name} number ${person.number} to phonebook`);
+//     mongoose.connection.close();
+//   });
+// }
+
+module.exports = mongoose.model("Person", personSchema);
